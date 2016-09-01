f18662d4c96d717bec5b679957139e952c56e782
Michael Gerbig <error418@users.noreply.github.com>
2016-09-01 12:25:09 +0200

    Update main.py
    add comment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from intertechno import Intertechno
 def main():
 	codeBuilder = Intertechno()
 	
+	# example: generate code for switching device with code A1 off
 	print(codeBuilder.translate('A', 1, False))
 
 if __name__ == "__main__":
